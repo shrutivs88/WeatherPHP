0e697c904ef444b76586e78e527c7c6a84e19dc5
shrutivs88 <shrutivs88@gmail.com>
2023-02-03 03:26:30 +0530

    applied css

---
+++ weather.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html> 
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+    <title>Weather App</title>
+    <body>
+        
+    </body>
+    </head>
+</html>
