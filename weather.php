f5653fdc1bd1216dfc9cf38c34357917f924f187
shrutivs88 <shrutivs88@gmail.com>
2023-02-03 05:52:58 +0530

    API key changed , html file deleted

--- weather.php
+++ weather.php
@@ -5,8 +5,9 @@
      
     if (isset($_GET['city'])) {
          
-     $urlContents = file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".urlencode($_GET['city']).",uk&appid=4b6cbadba309b7554491c5dc66401886");
-         
+    //  $urlContents = file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".urlencode($_GET['city']).",uk&appid=4b6cbadba309b7554491c5dc66401886");
+    $urlContents = file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=".urlencode($_GET['city']).",in&appid=63d957170f53f153512f19560d9b1137");
+   
         $weatherArray = json_decode($urlContents, true);
          
         if ($weatherArray['cod'] == 200) {
